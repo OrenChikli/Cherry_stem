1f34a250ecdb2e426eaf1d008836d348bc10a4e4
MrCumberdale <f.chikli@gmail.com>
2019-09-05 17:25:38 +0300

    has maskes, wrong version gets the paths off all the files in the folder, need to compare with csv instead

--- work/annotation/fruits_anno.py
+++ work/annotation/fruits_anno.py
@@ -6,19 +6,23 @@ from urllib.parse import urlparse
 import numpy as np
 import cv2
 from tqdm import tqdm
+import glob
 
 logger = logging.getLogger(__name__)
 
 class GoogleLabels:
 
-    def __init__(self):
+    def __init__(self, anno_path, src_images_path, dest_path, is_mask=False):
+        self.anno_path = anno_path
+        self.src_image_path = src_images_path
+        self.dest_path = dest_path
+        self.is_mask = is_mask
 
+
+        # exclude annotations for specific images
         self.exclude_labels = {'Clemantine6.JPG': [[0.78, 0.02], [0.87, 0.03], [0.93, 0.12], [0.94, 0.20]],
                                'Fig2.JPG': [[0.94, 0.42], [0.95, 0.67]],
                                'Mandarin5.JPG': [[0.2, 0.04], [0.05, 0.06], [0.13, 0.05], [0.05, 0.35], [0.09, 0.39]]}
-
-
-
 
     def get_points(self, image, normalized_vertices, exclude_labels_normalized=None):
         height, width = image.shape[:2]
@@ -52,78 +56,26 @@ class GoogleLabels:
 
         points = self.get_points(image, normalized_vertices, exclude_labels_normalized)
 
-        # mins = all_points.min(axis=0)
-        # maxs = all_points.max(axis=0)
 
         if points is not None:
             cv2.rectangle(image, tuple(points.min(axis=0)), tuple(points.max(axis=0)), color, 2)
 
     def draw_poly(self, image, normalized_vertices, is_closed, color, exclude_labels_normalized=None):
-        # height, width = image.shape[:2]
-        #
-        # # points = np.array([[(p['x'] if 'x' in p else 0) * width, (p['y'] if 'y' in p else 0) * height] for p in normalized_vertices])
-        #
-        # if height > width:
-        #     # points = np.array([[(p['y'] if 'y' in p else 0) * width, (p['x'] if 'x' in p else 0) * height] for p in normalized_vertices])
-        #     points = np.array([[p['y'] if 'y' in p else 0, p['x'] if 'x' in p else 0] for p in normalized_vertices])
-        # else:
-        #     # points = np.array([[(p['x'] if 'x' in p else 0) * width, (p['y'] if 'y' in p else 0) * height] for p in normalized_vertices])
-        #     points = np.array([[p['x'] if 'x' in p else 0, p['y'] if 'y' in p else 0] for p in normalized_vertices])
-        #
-        # # points *= [width, height]
-        #
-        # # if height > width:
-        # #     points = np.array([[(1 - (p['y'] if 'y' in p else 0)) * width, (p['x'] if 'x' in p else 0) * height] for p in
-        # #                        normalized_vertices])
-        # # else:
-        # #     points = np.array([[(1 - (p['x'] if 'x' in p else 0)) * width, (1 - (p['y'] if 'y' in p else 0)) * height] for p in normalized_vertices])
-        #
-        # # points = np.array([[(p['x'] if 'x' in p else 0) * width, (p['y'] if 'y' in p else 0) * height] for p in normalized_vertices])
-        # # points = np.array([[(p['x'] if 'x' in p else 0) * height, (p['y'] if 'y' in p else 0) * width] for p in normalized_vertices])
-        # # points = np.array([[(1 - (p['y'] if 'y' in p else 0)) * width, (p['x'] if 'x' in p else 0) * height] for p in normalized_vertices])
-        #
-        # # points = np.array([[p['x'] * width, p['y'] * height] for p in normalized_vertices], dtype=np.int32)
-        # # points = np.array([[p['x'] * height, p['y'] * width] for p in normalized_vertices], dtype=np.int32)
-        #
-        # points = np.round(points * [width, height]).astype(np.int32)
+
 
         points = self.get_points(image, normalized_vertices, exclude_labels_normalized)
 
         if points is not None:
-            # cv2.polylines(image, [points], False, color, 2)  # not closed
+
             cv2.polylines(image, [points], is_closed, color, 2)
-            # cv2.rectangle(image, tuple(points[0]), tuple(points[1]), color, 2)
+
 
     def draw_rectangle(self, image, normalized_vertices, color, exclude_labels_normalized=None):
-        # height, width = image.shape[:2]
-        #
-        # # points = np.array([[(p['x'] if 'x' in p else 0) * width, (p['y'] if 'y' in p else 0) * height] for p in normalized_vertices])
-        #
-        # if height > width:
-        #     points = np.array([[(p['y'] if 'y' in p else 0) * width, (p['x'] if 'x' in p else 0) * height] for p in
-        #                        normalized_vertices])
-        # else:
-        #     points = np.array([[(p['x'] if 'x' in p else 0) * width, (p['y'] if 'y' in p else 0) * height] for p in normalized_vertices])
-        # # if height > width:
-        # #     points = np.array([[(1 - (p['y'] if 'y' in p else 0)) * width, (p['x'] if 'x' in p else 0) * height] for p in
-        # #                        normalized_vertices])
-        # # else:
-        # #     points = np.array([[(1 - (p['x'] if 'x' in p else 0)) * width, (1 - (p['y'] if 'y' in p else 0)) * height] for p in normalized_vertices])
-        #
-        # # points = np.array([[(p['x'] if 'x' in p else 0) * width, (p['y'] if 'y' in p else 0) * height] for p in normalized_vertices])
-        # # points = np.array([[(p['x'] if 'x' in p else 0) * height, (p['y'] if 'y' in p else 0) * width] for p in normalized_vertices])
-        # # points = np.array([[(1 - (p['y'] if 'y' in p else 0)) * width, (p['x'] if 'x' in p else 0) * height] for p in normalized_vertices])
-        #
-        # # points = np.array([[p['x'] * width, p['y'] * height] for p in normalized_vertices], dtype=np.int32)
-        # # points = np.array([[p['x'] * height, p['y'] * width] for p in normalized_vertices], dtype=np.int32)
-        #
-        # points = np.round(points).astype(np.int32)
+
 
         points = self.get_points(image, normalized_vertices, exclude_labels_normalized)
 
-        # cv2.polylines(image, [points], False, color, 2)  # not closed
 
-        # cv2.polylines(image, [points], True, color, 2)  # closed
         if points is not None and points.shape[0] == 2 and points.shape[1] == 2:
             cv2.rectangle(image, tuple(points[0]), tuple(points[1]), color, 2)
 
@@ -133,7 +85,6 @@ class GoogleLabels:
         points = np.array([[(p['x'] if 'x' in p else 0) * width, (p['y'] if 'y' in p else 0) * height] for p in normalized_vertices])
 
         diff_square = (points - np.roll(points, -1, axis=0)) ** 2
-        # lengths = np.sort(np.sqrt(np.sum(diff_square, axis=1)))
 
         lengths = np.sqrt(np.sum(diff_square, axis=1))
         return lengths
@@ -143,39 +94,24 @@ class GoogleLabels:
         if anno_str == None:
             pass
 
-            # with open('/Users/roman/work/ClariFruit/python/ML/ml_training/tomatoes_detection/Tomatoes_result1_partial.json', 'r') as anno_file:
-            #     anno_str = anno_file.read()
-
         anno_dict = json.loads(anno_str)
 
         image_uri = anno_dict['image_payload']['image_uri']
 
-        folder_name = 'D:/Clarifruit/cherry_stem/data/images_orig/'
-        # file_name = urlparse(image_uri).path.replace('/MiscFruits/images/', folder_name)
-
         url_path = urlparse(image_uri).path
         file_name = os.path.basename(url_path)
 
-        # if file_name != 'Mandarin5.JPG':
-        #     return
-
-        # local_image_path = os.path.join(folder_name, file_name)
-        local_image_path = url_path.replace('/Cherry/images/', folder_name)
-        # img = cv2.imread(os.path.join(folder_name, file_name))
-
-        #img = Image(local_image_path)
-        #img.prepare_for_detection()
+        local_image_path = url_path.replace('/Cherry/images/', self.src_image_path)
 
         img = cv2.imread(local_image_path, cv2.IMREAD_UNCHANGED)
 
-        image = img.copy()
-        # image = img.original.copy()
-        #image = img.resized.copy()
-
-
-
-        all_fruits_dict = []
-        num = 0
+        if self.is_mask:
+            height, width = img.shape[:2]
+            image = np.zeros((height, width, 1), np.uint8)
+            mask_color = 255
+        else:
+            image = img.copy()
+            mask_color = (0,255,0)
 
         for annotation in anno_dict['annotations']:
 
@@ -185,69 +121,44 @@ class GoogleLabels:
             anno_type = next(iter(ann_val.keys()))
 
             normalized_vertices = ann_val[anno_type]['normalized_polyline']['normalized_vertices']
-            # normalized_vertices = annotation['annotation_value']['image_polyline_annotation']['normalized_polyline']['normalized_vertices']
-
-            label_class = ann_val[anno_type]['annotation_spec']['display_name']
-
-            # if 'image_bounding_poly_annotation' in ann_val:
-            #     normalized_vertices = annotation['annotation_value']['image_bounding_poly_annotation']['normalized_bounding_poly']['normalized_vertices']
-            #     # normalized_vertices = annotation['annotation_value']['image_polyline_annotation']['normalized_polyline']['normalized_vertices']
-            #
-            #     label_class = annotation['annotation_value']['image_bounding_poly_annotation']['annotation_spec']['display_name']
-            # elif 'image_polyline_annotation' in ann_val:
-            #     normalized_vertices = annotation['annotation_value']['image_polyline_annotation']['normalized_polyline']['normalized_vertices']
 
             try:
                 if anno_type == 'image_bounding_poly_annotation':
                     # draw_poly(image, normalized_vertices, True, (0, 255, 0))
-                    self.draw_poly_bounding_rect(image, normalized_vertices, (0, 255, 0), self.exclude_labels.get(file_name))
+                    self.draw_poly_bounding_rect(image, normalized_vertices, mask_color, self.exclude_labels.get(file_name))
                 elif anno_type == 'image_polyline_annotation':
-                    self.draw_poly(image, normalized_vertices, False, (0, 255, 0), self.exclude_labels.get(file_name))
+                    self.draw_poly(image, normalized_vertices, False, mask_color, self.exclude_labels.get(file_name))
 
-            except Exception as e:
+            except Exception:
                 logger.error('Cannot draw annotation!', exc_info=1)
-                # logger.exception()
 
-            # try:
-            #     if label_class == 'fruit':
-            #         draw_poly(image, normalized_vertices, (0, 255, 0))
-            #     if label_class == 'white ball':
-            #         draw_rectangle(image, normalized_vertices, (0, 255, 0))
-            #
-            # except Exception as e:
-            #     logger.error('Cannot draw polygon!')
-            #     # raise
+        cv2.imwrite(os.path.join(self.dest_path, file_name), image)
+        return local_image_path
 
 
-        save_folder = 'D:/Clarifruit/cherry_stem/data/anno_images'
-        # save_results_folder = '/Volumes/Samsung_T5/data/test_ref_images/labels_by_google1_results/'
 
-        # cv2.imshow(file_name, image)
-        cv2.imwrite(os.path.join(save_folder, file_name), image)
+
+
 
     def save_all_anno_images(self):
 
-        with open('D:/Clarifruit/cherry_stem/data/Cherry_cherry_stem_result.json', 'r') as anno_file:
-            anno_str_list = anno_file.readlines()
 
-        # with open('/Volumes/Samsung_T5/data/misc_fruits/MiscFruits_misc_fruits_ball.json', 'r') as anno_file:
-        #     anno_str_list = anno_file.readlines()
+        with open(self.anno_path, 'r') as ano_file:
+            anno_str_list = ano_file.readlines()
 
+        x= len(anno_str_list)
+
+        anno_paths = set()
         for anno_str in tqdm(anno_str_list):
-            self.load_anno(anno_str)
-            # break
+            anno_paths.add(self.load_anno(anno_str))
+
+        images_list = {os.path.join(self.src_image_path,f) for f in os.listdir(self.src_image_path)}
+        x=len(images_list)
+        no_anno = images_list.difference(anno_paths)
+
+        print("hello")
 
 
-    # def get_images_without_anno():
-    #
-    #     from glob import glob
-    #
-    #     all_files = glob('/Volumes/Samsung_T5/data/tomatoes_detection/images/*.jpg')
-    #     anno_files = glob('/Volumes/Samsung_T5/data/tomatoes_detection/images_anno/*.jpg')
-    #
-    #     all_file_names = set([os.path.basename(file_path) for file_path in all_files])
-    #     anno_file_names = set([os.path.basename(file_path) for file_path in anno_files])
-    #
-    #     diff = all_file_names - anno_file_names
+
 
 
--- work/main.py
+++ work/main.py
@@ -1,16 +1,16 @@
-from .annotation import fruits_anno
+from work.annotation import fruits_anno
+import glob
+
 def main():
+    ano_path = 'D:/Clarifruit/cherry_stem/data/Cherry_cherry_stem_result.json'
+    src_images_path = 'D:/Clarifruit/cherry_stem/data/images_orig/'
+    dest_path = 'D:/Clarifruit/cherry_stem/data/masks/'
 
-    gl = fruits_anno.GoogleLabels()
 
-    # get_images_without_anno()
 
-    # load_anno()
+    gl = fruits_anno.GoogleLabels(ano_path, src_images_path, dest_path, True)
+
     gl.save_all_anno_images()
-
-    cv2.waitKey()
-    cv2.destroyAllWindows()
-
 
 if __name__ == "__main__":
     main()
